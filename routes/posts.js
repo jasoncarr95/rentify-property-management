a388839421edd25eeb48589216f0ea448fdd41b9
Jason Carr <jason@jasoncarr.dev>
2022-09-14 22:41:09 -0400

    Add files from BUB template
    - Working template
    - Remove cloudinary & multer (no images to upload/store)

---
+++ routes/posts.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+const postsController = require("../controllers/posts");
+const { ensureAuth, ensureGuest } = require("../middleware/auth");
+
+//Post Routes - simplified for now
+router.get("/:id", ensureAuth, postsController.getPost);
+
+router.post("/createPost", upload.single("file"), postsController.createPost);
+
+router.put("/likePost/:id", postsController.likePost);
+
+router.delete("/deletePost/:id", postsController.deletePost);
+
+module.exports = router;
