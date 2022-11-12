67a07bdd1d5ac2afc99cc390d33d8228bb4e4bbb
jasoncarr95 <jason@jasoncarr.dev>
2022-11-12 15:21:18 -0500

    Clean up unused code & comments

--- routes/reports.js
+++ routes/reports.js
@@ -1,28 +1,15 @@
-// FROM TEMPLATE USING ROUTES/POSTS.JS
 const express = require("express");
 const router = express.Router();
-
-// These routes are coming from the exported module in this file
-const tenantsController = require('../controllers/tenants');
-
-const reportsController = require("../controllers/reports")
-
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
+// These routes are coming from the exported module in this file
+const reportsController = require("../controllers/reports");
+
 //Reports Routes - simplified for now
-router.get("/rentroll", ensureAuth, reportsController.getRentRoll)
+router.get("/rentroll", ensureAuth, reportsController.getRentRoll);
 
-
+//
+const tenantsController = require("../controllers/tenants");
 router.get("/:id", ensureAuth, tenantsController.getTenant);
-
-
-router.post("/createTenant", tenantsController.createTenant)
-
-// My Routes
-// router.get("/addTenant", ensureAuth, tenantsController.getForm)
-
-// router.put("/likePost/:id", postsController.likePost);
-
-// router.delete("/deletePost/:id", postsController.deletePost);
 
 module.exports = router;
--- routes/tenants.js
+++ routes/tenants.js
@@ -1,25 +1,14 @@
-// FROM TEMPLATE USING ROUTES/POSTS.JS
 const express = require("express");
 const router = express.Router();
+const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
 // These routes are coming from the exported module in this file
 const tenantsController = require("../controllers/tenants");
-
-const { ensureAuth, ensureGuest } = require("../middleware/auth");
-
-//Post Routes - simplified for now
 
 router.get("/", ensureAuth, tenantsController.getAllTenants);
 
 router.get("/:id", ensureAuth, tenantsController.getTenant);
 
-router.post("/createTenant", tenantsController.createTenant);
-
-// My Routes
-// router.get("/addTenant", ensureAuth, tenantsController.getForm)
-
-// router.put("/likePost/:id", postsController.likePost);
-
-// router.delete("/deletePost/:id", postsController.deletePost);
+router.post("/createTenant", ensureAuth, tenantsController.createTenant);
 
 module.exports = router;
